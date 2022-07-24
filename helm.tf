a70c42da9eb5491ea40024ab5479e5594a355b70
Taylor Dolezal <onlydole@gmail.com>
2022-07-24 09:35:47 -0700

    Add kube state metrics

---
+++ helm.tf
@@ -0,0 +1,8 @@
+# https://registry.terraform.io/providers/hashicorp/helm/latest/docs
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
---
+++ kube-state-metrics.tf
@@ -0,0 +1,18 @@
+# https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release
+resource "helm_release" "kube_state_metrics" {
+  name = "kube-state-metrics"
+
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-state-metrics"
+  namespace  = kubernetes_namespace.kube_state_metrics.metadata[0].name
+}
+
+# https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/namespace
+resource "kubernetes_namespace" "kube_state_metrics" {
+  metadata {
+    annotations = {
+      name = "kube-state-metrics"
+    }
+    name = "kube-state-metrics"
+  }
+}
