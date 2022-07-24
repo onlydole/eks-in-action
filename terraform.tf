05f4ba89efaedf940fa72c440258b9dd7dd2371e
Taylor Dolezal <onlydole@gmail.com>
2022-07-24 09:35:38 -0700

    Add Helm provider

--- terraform.tf
+++ terraform.tf
@@ -12,5 +12,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.22.0"
     }
+
+    # https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.6.0"
+    }
   }
 }
